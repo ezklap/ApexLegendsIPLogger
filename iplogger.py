2ad90074c6313357a55099cf4f860242dc70d106
modderfawker <48880299+modderfawker@users.noreply.github.com>
2020-05-11 13:25:04 +0400

    Update iplogger.py

--- iplogger.py
+++ iplogger.py
@@ -8,25 +8,34 @@ import subprocess
 import ipaddress
 import colorama
 
-
+ 
+ 
+ 
 global IPSTACK_API_KEY
 # Use this option if you want Geo Loction on the IP address get your free api key from https://ipstack.com
 # their databases arent so up to date but its a nice feature if you want to enable it
 IPSTACK_API_KEY = ''
-
+ 
 colorama.init()
-
+ 
 # Ping the server 1 time to get the response time from the server 
 def ping(command):
-	if ipaddress.ip_address(dest_ip).is_private == True:
+	if ipaddress.ip_address(dest_ip).is_private == True or dest_ip in ip_list:
 		return
 	else:
 		process = subprocess.Popen(command,stdout=subprocess.PIPE, shell=True)
 		proc_stdout = process.communicate()[0].strip()
-		print(proc_stdout.decode(),sep=' ', end='\n', flush=True)
-
-
-
+		#print(proc_stdout.decode(),sep=' ', end='\n', flush=True)
+		response_time_str = str(re.findall(r'time=(\d+)\w', proc_stdout.decode())[0])
+		response_time = int(response_time_str)
+		seen = set(ip_list)
+		if dest_ip not in seen:
+			seen.add(dest_ip)
+			ip_list.append(dest_ip)
+	return response_time
+ 
+ 
+ 
  
 # getting our local IP address
 def get_ip():
@@ -48,13 +57,13 @@ local_ip = get_ip()
 def pc(packet):
 	if packet.proto == 17:
 		udp = packet.payload
-
+ 
 # Clear the console on the next updated IP address for a cleaner console output
 def clear(): 
     print ("\x1b[2J")
  
-
-
+ 
+ 
 def function_1():
 	# looking for UDP packets sent to port 37005 Apex Legends servers 
 	x = sniff(filter="udp and portrange 37005-38515", prn=pc, store=1, count=1) # Do not capture local IP UDP it captures local UDP packets
@@ -65,12 +74,6 @@ def function_1():
 		dest_ip = z
 	else:
 		dest_ip = y
-
-	if ipaddress.ip_address(dest_ip).is_private == True:
-		return
-	else:
-		with open('ApexServerIPList.txt', 'a') as the_file:
-			the_file.write(dest_ip + '\n')
 	return dest_ip
  
  #  Geo Loction on the IP address you can get a free api key from their site just register and fill the global variable above on line 16
@@ -81,34 +84,53 @@ def ipStack(ip):
 		check_country = 'http://api.ipstack.com/{}?access_key={}&fields=country_name,country_code,city'.format(dest_ip,IPSTACK_API_KEY)
 		response = requests.get(check_country)
 		print('%r Game server IP: %r %s' % (strftime("%H:%M:%S", time.localtime()), dest_ip, response.json()))
-
+ 
 
 # We check for updated IP addresses while playing different matches 
 def check_ip(ip_check,ip2_check):
 	current = ip_check
 	new = ip2_check
-	if current != new:
+	if ipaddress.ip_address(dest_ip).is_private == True or ip_check == ip2_check:
+		return
+	else:
+		#elif current != new:
 		clear() 
-		print('IP: {}'.format(current), sep=' ', end='\n', flush=True)
-		ping('ping -n 1 {} | FIND "TTL="'.format(dest_ip)) # pings the server 
-		if IPSTACK_API_KEY == '':
+		response_time = ping('ping -n 1 {} | FIND "TTL="'.format(dest_ip)) # pings the server
+		print('Game Server IP: {} Response time: {}ms'.format(current,response_time), sep=' ', end='\n', flush=True)
+		if response_time is None:
 			return
+		elif response_time < 120:
+			
+			with open('Good-ApexServer.txt', 'a+') as the_file:
+				the_file.seek(0)
+				for line in the_file:
+					if dest_ip in line:
+						break
+				else:
+					the_file.write(dest_ip + '\n')
 		else:
-			ipStack(dest_ip) 
-
-		return current
+			with open('Lag-ApexServer.txt', 'a+') as the_file:
+				the_file.seek(0)
+				for line in the_file:
+					if dest_ip in line:
+						break
+				else:
+					the_file.write(dest_ip + '\n')
+			if IPSTACK_API_KEY == '':
+				return
+			else:
+				ipStack(dest_ip)
+		#return current
 	return current
-
-
-
-ip_flag = '0.0.0.0' 
-
+ 
+ 
+ 
+ip_flag = '0.0.0.0'
+global ip_list
+ip_list = []
 
 # This is the main function 
 while True:
 	a = str(function_1()) 
 	ip_flag = check_ip(a,ip_flag)  # function compares IP's if server changed or not
 	time.sleep(20)
-
-
- 
